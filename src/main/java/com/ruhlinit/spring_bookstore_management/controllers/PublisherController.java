b546aa8db6a517a363d4b62188d266f47f61215c
Jason Ruhlin <jsonruhls@users.noreply.github.com>
2024-07-10 19:55:13 -0400

    feat: add controller and general crud methods for publisher

---
+++ src/main/java/com/ruhlinit/spring_bookstore_management/controllers/PublisherController.java
@@ -0,0 +1,64 @@
+package com.ruhlinit.spring_bookstore_management.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ruhlinit.spring_bookstore_management.data.Publisher;
+import com.ruhlinit.spring_bookstore_management.repositories.PublisherRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/publishers")
+@RequiredArgsConstructor
+public class PublisherController {
+    private final PublisherRepository publisherRepository;
+
+    @PostMapping
+    public ResponseEntity<Publisher> addBook(@RequestBody Publisher publisher) {
+        Publisher savedPublisher = publisherRepository.save(publisher);
+        return ResponseEntity.ok(savedPublisher);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Publisher> getBook(@PathVariable Long id) {
+        Optional<Publisher> publisher = publisherRepository.findById(id);
+        return publisher.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public List<Publisher> getAllPublishers() {
+        return publisherRepository.findAll();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Publisher> updatePublisher(@PathVariable Long id, @RequestBody Publisher publisher) {
+        return publisherRepository.findById(id)
+                .map(existingPublisher -> {
+                    existingPublisher.setName(publisher.getName());
+                    existingPublisher.setAddress(publisher.getAddress());
+                    Publisher updatedPublisher = publisherRepository.save(existingPublisher);
+                    return ResponseEntity.ok(updatedPublisher);
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePublisher(@PathVariable Long id) {
+        if (publisherRepository.existsById(id)) {
+            publisherRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
