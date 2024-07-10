1e56bb76f9d21818c84eccd38421984ca1f571e2
Jason Ruhlin <jsonruhls@users.noreply.github.com>
2024-07-10 16:49:59 -0400

    feat: initialize spring app

---
+++ src/main/java/com/ruhlinit/spring_bookstore_management/SpringBookstoreManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ruhlinit.spring_bookstore_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBookstoreManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBookstoreManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ruhlinit/spring_bookstore_management/SpringBookstoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruhlinit.spring_bookstore_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBookstoreManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
