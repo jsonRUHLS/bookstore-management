df5319865735475acc94beed982e89272abecdf5
Jason Ruhlin <jsonruhls@users.noreply.github.com>
2024-07-10 17:07:19 -0400

    feat: create book entity and repository

---
+++ src/main/java/com/ruhlinit/spring_bookstore_management/data/Book.java
@@ -0,0 +1,17 @@
+package com.ruhlinit.spring_bookstore_management.data;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "books")
+@Data
+public class Book {
+    @Id @GeneratedValue
+    private Long id;
+    private String title;
+    private String isbn;
+}
