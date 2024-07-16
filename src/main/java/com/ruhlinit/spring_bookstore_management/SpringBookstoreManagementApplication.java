ecdf9ee43e95d4c29229391d55d2d0baecd75896
Jason Ruhlin <jsonruhls@users.noreply.github.com>
2024-07-16 19:48:30 -0400

    config: enable spring caching

--- src/main/java/com/ruhlinit/spring_bookstore_management/SpringBookstoreManagementApplication.java
+++ src/main/java/com/ruhlinit/spring_bookstore_management/SpringBookstoreManagementApplication.java
@@ -2,8 +2,10 @@ package com.ruhlinit.spring_bookstore_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SpringBookstoreManagementApplication {
 
 	public static void main(String[] args) {
