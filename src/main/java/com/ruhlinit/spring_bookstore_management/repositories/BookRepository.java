30c8a7fe960524a13556a892b0de2967f32168f3
Jason Ruhlin <jsonruhls@users.noreply.github.com>
2024-07-16 20:00:58 -0400

    feat: making book get operations cacheable via setting in repository for books

--- src/main/java/com/ruhlinit/spring_bookstore_management/repositories/BookRepository.java
+++ src/main/java/com/ruhlinit/spring_bookstore_management/repositories/BookRepository.java
@@ -1,11 +1,20 @@
 package com.ruhlinit.spring_bookstore_management.repositories;
 
+import java.util.Optional;
+
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ruhlinit.spring_bookstore_management.data.Book;
 
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    @SuppressWarnings("null")
+    @Override
+    @Cacheable("books")
+    Optional<Book> findById(Long id);
 
 }
